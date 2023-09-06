0d983033174db93c785d61aad8c7c6c3ef1a8f30
Shivam <shivamkasoudhan7@gmail.com>
2023-09-06 19:02:01 +0530

    db connect

--- server.js
+++ server.js
@@ -4,13 +4,29 @@
 import express from "express";
 import dotenv from 'dotenv';
 import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
 
+
+import connectDB from "./config/db.js";
+import testRoutes from './routes/testRoutes.js' 
 dotenv.config()
-const app= express();
 
-app.get("/",(req,res)=>{
-  res.send("<h1> Job protal</h1>");
-});
+// db connection
+connectDB();
+
+//rest object
+const app = express();
+
+
+//middelwares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+//routes
+app.use("/api/v1/test", testRoutes);
 
 const PORT=process.env.PORT || 8080;
 
